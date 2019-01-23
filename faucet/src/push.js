e69fd7f212b29a4c4cc1e7404d3b22ac05cf7ce2
triomino <3150102227@zju.edu.cn>
2019-01-23 17:27:34 +0800

    auto deploy

--- faucet/src/push.js
+++ faucet/src/push.js
@@ -20,13 +20,14 @@ function runScriptWithPushEvent(e) {
 
 
   if (ref === 'refs/heads/master' || shouldDeploy(commits)) {
-    console.log('rebuilding')
+    console.log('building and deploying')
     shell.exec(`git fetch origin ${ref} && git reset --hard ${after}`)
     // currently any push will cause all images to rebuild
-    for (let project in buildScripts) {
-      shell.exec(buildScripts[project])
-    }
-    console.log('building complete')
+    // for (let project in buildScripts) {
+    //  shell.exec(buildScripts[project])
+    // }
+    shell.exec('cd .. && docker-compose build && docker-compose push && docker stack deploy -c docker-compose.yml app')
+    console.log('complete')
   }
 }
 
